c0190f1ec27b2b86b5ff44dea5046ab2d6bee01b
lexi040 <lexi@alexandra04>
2025-05-02 11:37:58 +0200

    comportement selon tp1

--- robot_challenger.py
+++ robot_challenger.py
@@ -1,8 +1,8 @@
 # Projet "robotique" IA&Jeux 2025
 #
 # Binome:
-#  Prénom Nom No_étudiant/e : _________
-#  Prénom Nom No_étudiant/e : _________
+#  Prénom Nom No_étudiant/e : Bolea Alexandra
+#  Prénom Nom No_étudiant/e : Tonati Enzo
 #
 # check robot.py for sensor naming convention
 # all sensor and motor value are normalized (from 0.0 to 1.0 for sensors, -1.0 to +1.0 for motors)
@@ -24,7 +24,24 @@ class Robot_player(Robot):
         super().__init__(x_0, y_0, theta_0, name="Robot "+str(self.robot_id), team=self.team_name)
 
     def step(self, sensors, sensor_view=None, sensor_robot=None, sensor_team=None):
-        translation = sensors[sensor_front]
-        rotation = 1.0 * sensors[sensor_front_left] - 1.0 * sensors[sensor_front_right] + (random.random()-0.5)*0.1
-        return translation, rotation, False
+        front = 0
+        front_left = 1
+        front_right = 7
+        #eviter les murs
+        wall = [sensors[i] if sensor_view[i] == 1 else 1.0 for i in range(8)]
+        if wall[front] < 1.0:
+            translation = 0.7 * wall[front]
+            rotation = 0.5 * (wall[front_left] - wall[front_right])
+            return translation, rotation, False
 
+        # eviter les autres robots de son equipe
+        bot = [sensors[i] if sensor_view[i] == 2 and sensor_team[i] != self.team else 1.0 for i in range(8)]
+        if bot[front] < 1.0:
+            translation = (1.0 - bot[front]) * 0.7
+            rotation = 0.5 * (bot[front_right] - bot[front_left])
+            return translation, rotation, False
+
+        # tout droit
+        translation = 0.5
+        rotation = (random.random() - 0.5) * 0.1
+        return translation, rotation, False
